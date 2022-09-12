34e4ebfe8c036a0e7579e9baf8a9ceacb292ee15
Andrej Taneski <andrejtaneski7@gmail.com>
2022-09-12 20:31:43 +0200

    Add address saving support

--- contracts/CryptoExchange.sol
+++ contracts/CryptoExchange.sol
@@ -1,11 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+struct Address {
+    string name;
+    address payable _address;
+}
+
 contract CryptoExchange {
+
+    mapping (address => Address[]) addressMap;
 
     function sendEther(address payable _to) public payable {
         (bool sent, bytes memory data) = _to.call{value: msg.value}("");
         require(sent, "Failed to send Ether");
+    }
+
+    function saveAddress(string memory name, address payable _to) public returns(Address[] memory) {
+        addressMap[msg.sender].push(Address(name,_to));
+        return addressMap[msg.sender];
+    }
+
+    function getAddressList() public view returns(Address[] memory) {
+        return addressMap[msg.sender];
     }
     
 }
